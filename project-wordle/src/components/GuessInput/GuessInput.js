368162b45c24efa2bc3a4267a2aee253cb51846e
Alice Wood <125405269+aliceswood@users.noreply.github.com>
2024-02-21 14:47:28 +0000

    regex edit and tweaks

--- project-wordle/src/components/GuessInput/GuessInput.js
+++ project-wordle/src/components/GuessInput/GuessInput.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 
 function GuessInput() {
+  const [guess, setGuess] = useState("");
 
   function handleSubmit(event) {
       event.preventDefault();
       console.info({guess})
       setGuess('')
   }
-	const [guess, setGuess] = useState("");
 	return (
 		<>
 			<form className="guess-input-wrapper" 
@@ -16,13 +16,14 @@ function GuessInput() {
           Enter guess: 
         </label>
 				<input 
+          required
           id="guess-input" 
           type="text"
           value={guess} 
           onChange={event => {
             setGuess((event.target.value).toUpperCase())
           }}
-          pattern="[A-Z]{5}"
+          pattern="[a-zA-Z]{5}"
           title="Must be a 5 letter word"
           />
 			</form>
