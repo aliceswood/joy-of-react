b95ee47f38c45cb55ceb37f78be789df287138f3
Alice Wood <125405269+aliceswood@users.noreply.github.com>
2024-02-21 14:41:54 +0000

    handleSubmit function

--- project-wordle/src/components/GuessInput/GuessInput.js
+++ project-wordle/src/components/GuessInput/GuessInput.js
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 
 function GuessInput() {
+
+  function handleSubmit(event) {
+      event.preventDefault();
+      console.info({guess})
+      setGuess('')
+  }
 	const [guess, setGuess] = useState("");
 	return (
 		<>
 			<form className="guess-input-wrapper" 
-        onSubmit={(event) => {
-        event.preventDefault();
-        console.info({guess})
-        setGuess('')
-      }}>
+        onSubmit={handleSubmit}>
 				<label htmlFor="guess-input">
           Enter guess: 
         </label>
