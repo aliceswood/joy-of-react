bee742f1c6e2319a42a708bc9ceb146f3713fdc4
Alice Wood <125405269+aliceswood@users.noreply.github.com>
2024-02-23 16:06:05 +0000

    styling for letter validation

--- project-wordle/src/components/Guess/Guess.js
+++ project-wordle/src/components/Guess/Guess.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React from "react";
+import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers";
 
-import { range } from '../../utils';
+function Cell({ letter, status }) {
+  const className = status
+  ? `cell ${status}`
+  : 'cell';
 
-function Guess({ value }) {
-  return (
-    <p className="guess">
-      {range(5).map((num) => (
-        <span key={num} className="cell">
-          {value ? value[num] : undefined}
-        </span>
-      ))}
-    </p>
-  );
+	return (
+  <span className={className}>
+    {letter}
+  </span> 
+  )
+}
+
+function Guess({ value, answer }) {
+	const result = checkGuess(value, answer);
+
+
+	return (
+		<p className="guess">
+			{range(5).map((num) => (
+				<Cell
+					key={num}
+					letter={result ? result[num].letter : undefined}
+					status={result ? result[num].status : undefined}
+				/>
+			))}
+		</p>
+	);
 }
 
 export default Guess;
