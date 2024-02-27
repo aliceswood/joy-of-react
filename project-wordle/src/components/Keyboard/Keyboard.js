93ded253cb3af5891841ae79a461e4e9bfc3bce3
Alice Wood <125405269+aliceswood@users.noreply.github.com>
2024-02-27 15:59:22 +0000

    uppercase keyboard to fix coloring

--- project-wordle/src/components/Keyboard/Keyboard.js
+++ project-wordle/src/components/Keyboard/Keyboard.js
@@ -1,8 +1,8 @@
 import React from "react";
 const keyboardRows = [
-	["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
-	["a", "s", "d", "f", "g", "h", "j", "k", "l"],
-	["z", "x", "c", "v", "b", "n", "m"],
+	["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+	["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+	["Z", "X", "C", "V", "B", "N", "M"],
 ];
 
 function getStatusByLetter(validatedGuesses) {
@@ -36,7 +36,7 @@ function getStatusByLetter(validatedGuesses) {
 
 function Keyboard({ validatedGuesses }) {
 	const statusByLetter = getStatusByLetter(validatedGuesses);
-  
+  console.log(statusByLetter)
 
 	return (
 		<div className="keyboard">
@@ -47,6 +47,7 @@ function Keyboard({ validatedGuesses }) {
 							key={letter}
 							className={`letter ${statusByLetter[letter] || ''}`}
 						>
+              {console.log(letter)}
 							{letter}
 						</div>
 					))}
