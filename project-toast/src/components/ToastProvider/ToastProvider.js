5ba29d8bbc74a26362a44b4aed2b630727c8c0b1
Alice Wood <125405269+aliceswood@users.noreply.github.com>
2024-04-09 13:42:20 +0100

    correct values passed through ToastContext

--- project-toast/src/components/ToastProvider/ToastProvider.js
+++ project-toast/src/components/ToastProvider/ToastProvider.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const ToastContext = React.createContext();
 
 function ToastProvider({ children }) {
 	const [toastList, setToastList] = useState([]);
 
-	return <ToastContext.Provider value={{ toasts }}>{children}</ToastContext.Provider>;
+	return <ToastContext.Provider value={{ toastList, setToastList }}>{children}</ToastContext.Provider>;
 }
 
 export default ToastProvider;
