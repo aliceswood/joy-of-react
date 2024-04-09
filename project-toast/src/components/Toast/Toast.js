431a63dda7037e4b03d277798b8b05318f43edd7
Alice Wood <125405269+aliceswood@users.noreply.github.com>
2024-04-09 11:33:05 +0100

    icon dynamic based on variant selected

--- project-toast/src/components/Toast/Toast.js
+++ project-toast/src/components/Toast/Toast.js
@@ -1,38 +1,32 @@
-import React from 'react';
-import {
-  AlertOctagon,
-  AlertTriangle,
-  CheckCircle,
-  Info,
-  X,
-} from 'react-feather';
+import React from "react";
+import { AlertOctagon, AlertTriangle, CheckCircle, Info, X } from "react-feather";
 
-import VisuallyHidden from '../VisuallyHidden';
+import VisuallyHidden from "../VisuallyHidden";
 
-import styles from './Toast.module.css';
+import styles from "./Toast.module.css";
 
 const ICONS_BY_VARIANT = {
-  notice: Info,
-  warning: AlertTriangle,
-  success: CheckCircle,
-  error: AlertOctagon,
+	notice: Info,
+	warning: AlertTriangle,
+	success: CheckCircle,
+	error: AlertOctagon,
 };
 
-function Toast({children, variant}) {
-  return (
-    <div className={`${styles.toast} ${styles[variant]}`}>
-      <div className={styles.iconContainer}>
-        <Info size={24} />
-      </div>
-      <p className={styles.content}>
-        {children}
-      </p>
-      <button className={styles.closeButton}>
-        <X size={24} />
-        <VisuallyHidden>Dismiss message</VisuallyHidden>
-      </button>
-    </div>
-  );
+function Toast({ children, variant }) {
+	const Icon = ICONS_BY_VARIANT[variant];
+
+	return (
+		<div className={`${styles.toast} ${styles[variant]}`}>
+			<div className={styles.iconContainer}>
+				<Icon size={24} />
+			</div>
+			<p className={styles.content}>{children}</p>
+			<button className={styles.closeButton}>
+				<X size={24} />
+				<VisuallyHidden>Dismiss message</VisuallyHidden>
+			</button>
+		</div>
+	);
 }
 
 export default Toast;
