977d86d7977b25a8f275fa3f7ecf60e541d689db
Alice Wood <125405269+aliceswood@users.noreply.github.com>
2024-04-09 13:30:52 +0100

    Add ToastProvider and wrap App

--- project-toast/src/components/ToastProvider/ToastProvider.js
+++ project-toast/src/components/ToastProvider/ToastProvider.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React from "react";
 
-function ToastProvider() {
-  return <div></div>;
+export const ToastContext = React.createContext();
+
+function ToastProvider({ children }) {
+	const [toastList, setToastList] = useState([]);
+
+	return <ToastContext.Provider value={{ toasts }}>{children}</ToastContext.Provider>;
 }
 
 export default ToastProvider;
