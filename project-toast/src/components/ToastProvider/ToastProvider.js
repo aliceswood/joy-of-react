5cc665975f2f02b21862caa03bdddc657919217f
Alice Wood <125405269+aliceswood@users.noreply.github.com>
2024-04-09 15:20:46 +0100

    escape dismises all toasts

--- project-toast/src/components/ToastProvider/ToastProvider.js
+++ project-toast/src/components/ToastProvider/ToastProvider.js
@@ -1,11 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export const ToastContext = React.createContext();
 
 function ToastProvider({ children }) {
 	const [toastList, setToastList] = useState([]);
 
-	return <ToastContext.Provider value={{ toastList, setToastList }}>{children}</ToastContext.Provider>;
+	useEffect(() => {
+		function handleKeyDown(event) {
+			if (event.code === "Escape") {
+				setToastList([]);
+			}
+		}
+		window.addEventListener("keydown", handleKeyDown);
+
+		return () => {
+			window.removeEventListener("keydown", handleKeyDown);
+		};
+	}, []);
+
+	
+	return (
+		<ToastContext.Provider value={{ toastList, setToastList }}>{children}</ToastContext.Provider>
+	);
 }
 
 export default ToastProvider;
